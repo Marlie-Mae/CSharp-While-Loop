d3cad14ecee98f9c4326f1dd4f2a596cffed7956
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2024-04-04 16:00:32 +0800

    Add files via upload

---
+++ Item3.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program 
+{
+	static void Main(string[] args)
+	{
+		int i = 1;
+
+        while (i <= 100) 
+        {
+            Console.WriteLine(i);
+            i += 1;
+        } 
+	}
+}
